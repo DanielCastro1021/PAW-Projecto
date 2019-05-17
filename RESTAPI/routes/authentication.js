60274a7df24e2e069655f8de6b1aa9610247a045
Daniel Castro <danielcastrofafe@gmail.com>
2019-05-17 14:51:01 +0100

    Files name change

--- RESTAPI/routes/authentication.js
+++ RESTAPI/routes/authentication.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 //var verifyToken = require('../controllers/VerifyToken');
 var verifyRole = require('../controllers/VerifyRole');
-var authController = require('../controllers/AuthController');
+var authController = require('../controllers/AuthenticationController');
 
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
