e86d20f3ea896386c417d9f754333f4e4b121413
Daniel Castro <danielcastrofafe@gmail.com>
2019-05-21 14:39:39 +0100

    Updated User Model

--- RESTAPI/models/user.js
+++ RESTAPI/models/user.js
@@ -3,6 +3,9 @@ var UserSchema = new mongoose.Schema(
   {
     username: { type: String, unique: true, required: true },
     password: { type: String },
+    fullname: { type: String, required: true },
+    nif: { type: Number, min: 100000000, max: 999999999, required: true },
+    iban: { type: String, required: true },
     role: { type: String, default: 'user' },
     coordinates: { latitude: Number, longitude: Number },
     address: { type: String }
